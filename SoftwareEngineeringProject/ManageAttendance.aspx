4d52d207c82733b829418fbcb0086e6368286ee6
wi jie <p23015803@student.newinti.edu.my>
2026-06-22 19:01:48 +0800

    ManageAttendance add in. (Third Commit)

---
+++ SoftwareEngineeringProject/ManageAttendance.aspx
@@ -0,0 +1,146 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ManageAttendance.aspx.cs" Inherits="SoftwareEngineeringProject.ManageAttendance" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Attendance</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="ManageAttendance.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="attendance-header">
+
+            <asp:Label ID="lblCourse"
+                runat="server"
+                CssClass="course-header">
+            </asp:Label>
+
+        </div>
+
+        <div class="attendance-menu">
+
+            <asp:Button ID="btnBackDashboard"
+                runat="server"
+                Text="← Dashboard"
+                PostBackUrl="~/LecturerDashboard.aspx"
+                CssClass="menu-btn" />
+
+            <asp:Button ID="btnAttendance"
+                runat="server"
+                Text="Attendance"
+                CssClass="menu-btn active-btn" />
+
+            <asp:Button ID="btnAttendanceReport"
+                runat="server"
+                Text="Attendance Report"
+                PostBackUrl="~/AttendanceReport.aspx"
+                CssClass="menu-btn" />
+
+            <asp:Button ID="btnMarks"
+                runat="server"
+                Text="Marks"
+                PostBackUrl="~/EnterMarks.aspx"
+                CssClass="menu-btn" />
+
+            <asp:Button ID="btnMarksReport"
+                runat="server"
+                Text="Marks Report"
+                PostBackUrl="~/MarksReport.aspx"
+                CssClass="menu-btn" />
+
+        </div>
+
+        <br />
+
+        <div class="attendance-container">
+
+            <asp:TextBox ID="txtDate"
+                runat="server"
+                TextMode="Date"
+                CssClass="form-control">
+            </asp:TextBox>
+
+            <asp:Label ID="lblDateError"
+                runat="server"
+                ForeColor="Red">
+            </asp:Label>
+
+            <br /> <br />
+
+            <asp:Label ID="lblStudentCount"
+                runat="server"
+                CssClass="student-count">
+            </asp:Label>
+
+            <asp:GridView ID="gvStudents"
+                runat="server"
+                AutoGenerateColumns="False"
+                DataKeyNames="EnrollmentID">
+
+                <Columns>
+
+                    <asp:BoundField
+                        DataField="StudentID"
+                        HeaderText="Student ID" />
+
+                    <asp:BoundField
+                        DataField="FullName"
+                        HeaderText="Student Name" />
+
+                    <asp:BoundField
+                        DataField="Email"
+                        HeaderText="Email" />
+
+                    <asp:TemplateField
+                        HeaderText="Attendance">
+
+                        <ItemTemplate>
+
+                            <asp:DropDownList ID="ddlStatus"
+                                runat="server"
+                                CssClass = "attendance-dropdown">
+
+                                <asp:ListItem Text="Present" Value="Present" />
+                                <asp:ListItem Text="Absent" Value="Absent" />
+                                <asp:ListItem Text="Late" Value="Late" />
+
+                            </asp:DropDownList>
+
+                        </ItemTemplate>
+
+                    </asp:TemplateField>
+
+                </Columns>
+
+            </asp:GridView>
+
+            <br />
+
+            <asp:Label ID="lblNoStudents"
+                runat="server"
+                ForeColor="Red"
+                Font-Bold="true">
+            </asp:Label>
+
+            <br />
+
+            <asp:Button ID="btnSave"
+                runat="server"
+                Text="Save Attendance"
+                CssClass="btn btn-success"
+                OnClick="btnSave_Click" />
+
+            <br /><br />
+
+            <asp:Label ID="lblMessage"
+                runat="server">
+            </asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SoftwareEngineeringProject/ManageAttendance.aspx.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SoftwareEngineeringProject
+{
+    public partial class ManageAttendance : System.Web.UI.Page
+    {
+        string connStr =
+        ConfigurationManager.ConnectionStrings["CollegeManagementSystem"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["SelectedCourseID"] == null)
+                {
+                    Response.Redirect("LecturerDashboard.aspx");
+                }
+
+                LoadCourseInfo();
+                LoadStudents();
+            }
+        }
+
+        private void LoadCourseInfo()
+        {
+            using (SqlConnection conn =
+                new SqlConnection(connStr))
+            {
+                string query =
+                @"SELECT CourseCode, CourseName FROM Courses WHERE CourseID=@CourseID";
+
+                SqlCommand cmd =
+                    new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue(
+                    "@CourseID",
+                    Convert.ToInt32(Session["SelectedCourseID"]));
+
+                conn.Open();
+
+                SqlDataReader dr =
+                    cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    lblCourse.Text =
+                        dr["CourseCode"] + " : " +
+                        dr["CourseName"];
+                }
+            }
+        }
+
+        private void LoadStudents()
+        {
+            using (SqlConnection conn =
+                new SqlConnection(connStr))
+            {
+                string query =
+                @"SELECT E.EnrollmentID, S.StudentID, U.FullName, U.Email
+
+                FROM Enrollments E
+
+                INNER JOIN Students S
+                ON E.StudentID = S.StudentID
+
+                INNER JOIN Users U
+                ON S.UserID = U.UserID
+
+                WHERE E.CourseID=@CourseID";
+
+                SqlCommand cmd =
+                    new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue(
+                    "@CourseID",
+                    Convert.ToInt32(Session["SelectedCourseID"]));
+
+                SqlDataAdapter da =
+                    new SqlDataAdapter(cmd);
+
+                DataTable dt =
+                    new DataTable();
+
+                da.Fill(dt);
+
+                lblStudentCount.Text = "Total Students: " + dt.Rows.Count;
+
+                if (dt.Rows.Count == 0)
+                {
+                    lblNoStudents.Text =
+                        "No students enrolled in this course.";
+
+                    gvStudents.Visible = false;
+                    btnSave.Visible = false;
+                }
+                else
+                {
+                    lblNoStudents.Text = "";
+
+                    gvStudents.Visible = true;
+                    btnSave.Visible = true;
+
+                    gvStudents.DataSource = dt;
+                    gvStudents.DataBind();
+                }
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (gvStudents.Rows.Count == 0)
+            {
+                lblMessage.Text =
+                    "No students available for attendance.";
+
+                return;
+            }
+
+            lblDateError.Text = "";
+
+            if (string.IsNullOrWhiteSpace(txtDate.Text))
+            {
+                lblDateError.Text =
+                    "Please select an attendance date.";
+
+                return;
+            }
+
+            DateTime attendanceDate = Convert.ToDateTime(txtDate.Text);
+
+            using (SqlConnection con =
+                new SqlConnection(connStr))
+            {
+                con.Open();
+
+                foreach (GridViewRow row in gvStudents.Rows)
+                {
+                    int enrollmentID = Convert.ToInt32(row.Cells[0].Text);
+
+                    DropDownList ddl =
+                    (DropDownList)row.FindControl(
+                    "ddlStatus");
+
+                    string status =
+                    ddl.SelectedValue;
+
+                    string query = @"
+                    IF EXISTS
+                    (
+                        SELECT 1
+                        FROM Attendance
+                        WHERE EnrollmentID = @EnrollmentID
+                        AND AttendanceDate = @Date
+                    )
+                    BEGIN
+                        UPDATE Attendance
+                        SET Status = @Status
+                        WHERE EnrollmentID = @EnrollmentID
+                        AND AttendanceDate = @Date
+                    END
+                    ELSE
+                    BEGIN
+                        INSERT INTO Attendance
+                        (
+                            EnrollmentID,
+                            AttendanceDate,
+                            Status
+                        )
+                        VALUES
+                        (
+                            @EnrollmentID,
+                            @Date,
+                            @Status
+                        )
+                    END";
+
+                    SqlCommand cmd =
+                    new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@EnrollmentID", enrollmentID);
+
+                    cmd.Parameters.AddWithValue("@Date", attendanceDate);
+
+                    cmd.Parameters.AddWithValue("@Status", status);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                lblMessage.Text =
+                "Attendance saved successfully.";
+            }
+        }
+    }
+}
