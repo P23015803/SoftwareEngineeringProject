f4bb4a2271b18a092eea16aff4d7e4b82fc6d882
wi jie <p23015803@student.newinti.edu.my>
2026-06-23 16:18:25 +0800

    Student Assignment Added for Additional Page in Enter Marks, Use for marking student assignment marks. Commit

---
+++ SoftwareEngineeringProject/StudentAssignments.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentAssignments.aspx.cs" Inherits="SoftwareEngineeringProject.StudentAssignments" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Student Assignments</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+        <link href="StudentAssignments.css" rel="stylesheet" />
+    </head>
+
+    <body>
+
+        <form id="form1" runat="server">
+
+            <div class="assignment-header">
+
+                <asp:Label ID="lblCourse"
+                    runat="server"
+                    CssClass="course-header">
+                </asp:Label>
+
+            </div>
+
+            <div class="assignment-menu">
+
+                <asp:Button ID="btnBackMarks"
+                    runat="server"
+                    Text="← Enter Marks"
+                    PostBackUrl="~/EnterMarks.aspx"
+                    CssClass="menu-btn" />
+
+            </div>
+
+            <br />
+
+            <div class="student-box">
+
+                <h4>Student Information</h4>
+
+                <asp:Label ID="lblStudent"
+                    runat="server"
+                    CssClass="student-label">
+                </asp:Label>
+
+            </div>
+
+            <br />
+
+            <!-- Assignment Table -->
+
+            <div class="assignment-table-wrapper">
+
+                <asp:GridView ID="gvAssignments"
+                    runat="server"
+                    AutoGenerateColumns="False"
+                    DataKeyNames="SubmissionID"
+                    CssClass="table table-bordered table-striped">
+
+                    <Columns>
+
+                        <asp:BoundField
+                            DataField="AssignmentTitle"
+                            HeaderText="Assignment Title" />
+
+                        <asp:TemplateField
+                            HeaderText="Submitted File">
+
+                            <ItemTemplate>
+
+                                <asp:HyperLink ID="lnkFile"
+                                    runat="server"
+                                    Text='<%# Eval("FileName") %>'
+                                    NavigateUrl='<%# Eval("FilePath") %>'
+                                    Target="_blank">
+                                </asp:HyperLink>
+
+                            </ItemTemplate>
+
+                        </asp:TemplateField>
+
+                        <asp:TemplateField
+                            HeaderText="Marks">
+
+                            <ItemTemplate>
+
+                                <asp:TextBox ID="txtMarks"
+                                    runat="server"
+                                    CssClass="form-control"
+                                    Width="100px"
+                                    Text='<%# Eval("Marks") %>'>
+                                </asp:TextBox>
+
+                            </ItemTemplate>
+
+                        </asp:TemplateField>
+
+                    </Columns>
+
+                </asp:GridView>
+
+            </div>
+
+            <br />
+
+            <asp:Button ID="btnSave"
+                runat="server"
+                Text="Save Assignment Marks"
+                CssClass="btn btn-success"
+                OnClick="btnSave_Click" />
+
+            <br /><br />
+
+            <asp:Label ID="lblMessage"
+                runat="server">
+            </asp:Label>
+
+        </form>
+
+    </body>
+</html>
---
+++ SoftwareEngineeringProject/StudentAssignments.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SoftwareEngineeringProject
+{
+    public partial class StudentAssignments : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["CollegeManagementSystem"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["SelectedEnrollmentID"] == null)
+                {
+                    Response.Redirect(
+                        "EnterMarks.aspx");
+                }
+
+                LoadStudentInfo();
+                LoadAssignments();
+            }
+        }
+
+        private void LoadStudentInfo()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query =
+                @"SELECT S.StudentID, U.FullName, C.CourseCode, C.CourseName
+
+                FROM Enrollments E
+
+                INNER JOIN Students S
+                ON E.StudentID = S.StudentID
+
+                INNER JOIN Users U
+                ON S.UserID = U.UserID
+
+                INNER JOIN Courses C
+                ON E.CourseID = C.CourseID
+
+                WHERE E.EnrollmentID = @EnrollmentID";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@EnrollmentID", Convert.ToInt32(Session["SelectedEnrollmentID"]));
+
+                conn.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    lblStudent.Text =
+                        dr["StudentID"] + " - " +
+                        dr["FullName"];
+
+                    lblCourse.Text =
+                        dr["CourseCode"] + " : " +
+                        dr["CourseName"];
+                }
+            }
+        }
+
+        private void LoadAssignments()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query =
+                @"SELECT S.SubmissionID, A.Title, S.FileName, S.FilePath, S.Marks
+
+                FROM AssignmentSubmissions S
+
+                INNER JOIN Assignments A
+                ON S.AssignmentID = A.AssignmentID
+
+                WHERE S.EnrollmentID = @EnrollmentID";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@EnrollmentID", Convert.ToInt32(Session["SelectedEnrollmentID"]));
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                if (dt.Rows.Count == 0)
+                {
+                    lblMessage.Text =
+                        "No assignment submissions found.";
+
+                    gvAssignments.Visible = false;
+                    btnSave.Visible = false;
+
+                    return;
+                }
+
+                gvAssignments.Visible = true;
+                btnSave.Visible = true;
+
+                gvAssignments.DataSource = dt;
+                gvAssignments.DataBind();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                foreach (GridViewRow row in gvAssignments.Rows)
+                {
+                    int submissionID = Convert.ToInt32(gvAssignments.DataKeys[row.RowIndex].Value);
+
+                    TextBox txtMarks = (TextBox)row.FindControl("txtMarks");
+
+                    decimal marks = string.IsNullOrWhiteSpace(txtMarks.Text)
+                    ? 0
+                    : Convert.ToDecimal(txtMarks.Text);
+
+                    string query =
+                    @"UPDATE AssignmentSubmissions
+
+                    SET Marks = @Marks
+
+                    WHERE SubmissionID =
+                    @SubmissionID";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@Marks", marks);
+
+                    cmd.Parameters.AddWithValue("@SubmissionID", submissionID);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                lblMessage.Text =
+                "Assignment marks saved successfully.";
+            }
+
+            LoadAssignments();
+        }
+    }
+}
